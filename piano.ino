73023d40455073d2a11e4a1a8e4d709a9df0c2d3
Vaicel <sarumanlol34@gmail.com>
2016-03-19 19:26:09 +0600

    add procedure - note

--- piano.ino
+++ piano.ino
@@ -21,6 +21,21 @@ Code made by Vaicel. 2016.
 #define tonePin 10
 #define del 100
 int digit;             //Включенный светодиод
+void note(int digitality, int tonote){
+	/***********************************
+	/Сначала проверяется, включен ли этот светодиод,
+	Если да, то продолжаем тянуть ноту
+	Если нет, то выключаем прошлый светодиод, включаем наш
+	И тянем ноту/
+	***********************************/
+	if(digit!=digitality){
+	    digitalWrite(digit, LOW);
+	    digit=digitality;
+	    tone(tonePin, tonote, del);
+	    digitalWrite(digit, HIGH);
+	}
+    tone(tonePin, tonote, del);
+}
 void setup()
 {
 	for(int i=2; i<10; i++){
@@ -29,84 +44,35 @@ void setup()
 }
 void loop()
 {
-	/***********************************
-	Далее программа состит из одинаковых блоков:
-	/Сначала проверяется, включен ли этот светодиод,
-	Если да, то продолжаем тянуть ноту
-	Если нет, то выключаем прошлый светодиод, включаем наш
-	И тянем ноту/
-	***********************************/
 	if(analogRead(A1)/45==aC4){
-		if(digit!=2){
-		   digitalWrite(digit, LOW);
-		   digit=2;
-	       tone(tonePin, NOTE_C4, del);
-	       digitalWrite(digit, HIGH);
-		}
-        tone(tonePin, NOTE_C4, del);
+		note(2, NOTE_C4);
 	}
 	else if(analogRead(A1)/45==aD4){
-		if(digit!=3){
-		   digitalWrite(digit, LOW);
-		   digit=3;
-	       tone(tonePin, NOTE_D4, del);
-	       digitalWrite(digit, HIGH);
-	    }
-	    tone(tonePin, NOTE_D4, del);
+		note(3, NOTE_D4);
 	}
 	else if(analogRead(A1)/45==aE4){
-		if(digit!=4){
-		   digitalWrite(digit, LOW);
-	       digit=4;
-	       tone(tonePin, NOTE_E4, del);
-	       digitalWrite(digit, HIGH);
-	    }
-	    tone(tonePin, NOTE_E4, del);
+		note(4, NOTE_E4);
+
 	}
 	else if(analogRead(A1)/45==aF4){
-		if(digit!=5){
-		   digitalWrite(digit, LOW);
-	       digit=5;
-	       tone(tonePin, NOTE_F4, del);
-	       digitalWrite(digit, HIGH);
-	    }
-	    tone(tonePin, NOTE_F4, del);
+		note(5, NOTE_F4);
+
 	}
 	else if(analogRead(A1)/45==aG4){
-		if(digit!=6){
-		   digitalWrite(digit, LOW);
-	       digit=6;
-	       tone(tonePin, NOTE_G4, del);
-	       digitalWrite(digit, HIGH);
-	    }
-	    tone(tonePin, NOTE_G4, del);
+		note(6, NOTE_G4);
+
 	}
 	else if(analogRead(A1)/45==aA4){
-		if(digit!=7){
-		   digitalWrite(digit, LOW);
-	       digit=7;
-	       tone(tonePin, NOTE_A4, del);
-	       digitalWrite(digit, HIGH);
-	    }
-	    tone(tonePin, NOTE_A4, del);
+		note(7, NOTE_A4);
+
 	}
 	else if(analogRead(A1)/45==aB4){
-		if(digit!=8){
-		   digitalWrite(digit, LOW);
-	       digit=8;
-	       tone(tonePin, NOTE_B4, del);
-	       digitalWrite(digit, HIGH);
-	    }
-	    tone(tonePin, NOTE_B4, del);
+		note(8, NOTE_B4);
+
 	}
 	else if(analogRead(A1)/45==aC5){
-		if(digit!=9){
-		   digitalWrite(digit, LOW);
-	       digit=9;
-	       tone(tonePin, NOTE_C5, del);
-	       digitalWrite(digit, HIGH);
-	    }
-	    tone(tonePin, NOTE_C5, del);
+		note(3, NOTE_C%);
+
 	}
 	else{
 		digitalWrite(digit, LOW); //выключение светодиода, при отпускании кнопки
