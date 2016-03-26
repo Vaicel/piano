f99fd7b8f615188eedeb6f2857f6584da5f1db88
dubkov <dubkov@skbrii.ru>
2016-03-26 15:15:37 +0600

    interrupt

--- piano.ino
+++ piano.ino
@@ -3,8 +3,9 @@ Code made by Vaicel. 2016.
 //dubkov branch
 ******************/
 
+// Частоты 
 
-#define NOTE_C4  262   //Дефайны нот
+#define NOTE_C4  262 
 #define NOTE_D4  294
 #define NOTE_E4  330
 #define NOTE_F4  349
@@ -12,6 +13,9 @@ Code made by Vaicel. 2016.
 #define NOTE_A4  440
 #define NOTE_B4  494
 #define NOTE_C5  523
+
+
+/*
 #define aC4 9		   //Дефайны значений кнопок, получаемые через аналоговый вход
 #define aD4 8
 #define aE4 7
@@ -21,42 +25,33 @@ Code made by Vaicel. 2016.
 #define aB4 3
 #define aC5 1
 #define kdel 100  //delay
-int digit,lastDigit,nowNote;
-int notes[8]={NOTE_C5,NOTE_B4,NOTE_A4,NOTE_G4,NOTE_F4,NOTE_E4,NOTE_D4,NOTE_C4};          //Включенный светодиод
-void note(int tonePin, int tonote, int del){
-	/***********************************
-	/Сначала проверяется, включен ли этот светодиод,
-	Если да, то продолжаем тянуть ноту
-	Если нет, то выключаем прошлый светодиод, включаем наш
-	И тянем ноту/
-	***********************************/
+*/
 
-	if(digit!=lastDigit){
-	    digitalWrite(lastDigit, LOW);
-	    lastDigit=digit;
-	    tone(tonePin, tonote, del);
-	    digitalWrite(digit, HIGH);
-	}
-    tone(tonePin, tonote, del);
+int notes = {
+	{1,2,3,4,5,6,7,8},
+	{2,3,4,5,6,7,8,1},
+	{3,4,5,6,7,8,1,2},
+	{4,5,6,7,8,1,2,3},
+	{5,6,7,8,1,2,3,4},
+	{6,7,8,1,2,3,4,5},
+	{7,8,1,2,3,4,5,6},
+	{8,1,2,3,4,5,6,7}
 }
-void start(){
-	if(analogRead(A1)/45!=1){
-		nowNote=(analogRead(A1)/45)-2;
-	}
-	else{
-		nowNote=(analogRead(A1)/45)-1;
-	}
-	digit=nowNote+2;
+
+volatile int mode = 0;
+
+void setup(){
+	attachInterrupt(digitalPinToInterrupt(2), switchMode, RISING);
 }
-void setup()
-{
-	for(int i=2; i<10; i++){
-	    pinMode(i, OUTPUT);
-	}
+
+void loop(){
+ 	
 }
-void loop()
-{	
-	start();
-	note(10,notes[nowNote],kdel);
-	delay(kdel);
+
+void switchMode(){
+	mode += 1;
+	if (mode == 3) mode = 0;
 }
+
+// выбор режима 
+
